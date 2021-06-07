75769ad2cc082ab2b34df6efe9fd8275beeb1911
Affteron <Lontaredal@outlook.com>
2021-06-07 21:52:34 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
+from datetime import datetime, timedelta
+from pathlib import Path
 import openpyxl
 import random
-from pathlib import Path
-from datetime import datetime, timedelta
 
-_COMPANY_NAME: list[str] = ["Компания_1", "Компания_2", "Компания_3"]
-_OPERATIONS: list[str] = ["Выплата зп", "Отплата налогов", "Закупка оборудования"]
-_TABLE_HEADER: list[str] = ["Компания", "Дата", "Тип операции", "Сумма"]
+
+_TABLE_INFORMATION: dict[str, tuple] = {
+    "Header": ("Компания", "Дата", "Тип операции", "Сумма"),
+    "Company_name": ("Компания_1", "Компания_2", "Компания_3"),
+    "Operations": ("Выплата зп", "Отплата налогов", "Закупка оборудования")
+}
+
 _CUSTOM_FIRST_TABLE_PATH: Path = Path(r".\first_table.xlsx")
 _CUSTOM_SECOND_TABLE_PATH: Path = Path(r".\second_table.xlsx")
 _MAX_ROW_NUMBER: int = 10000
-_MAX_COL_NUMBER: int = len(_TABLE_HEADER)
+_MAX_COL_NUMBER: int = len(_TABLE_INFORMATION["Header"])
 
 
-def create_table():
+def create_table(save_path: Path):
     wb = openpyxl.Workbook()
     ws = wb.active
     ws.title = "Информация по операциям"
-    for index, cell in enumerate(ws.iter_cols(max_row=1, max_col=_MAX_COL_NUMBER)):
-        cell[0].value = _TABLE_HEADER[index]
+
+    ws.append(_TABLE_INFORMATION["Header"])
 
     current_date = datetime.now()
-    for row in ws.iter_rows(min_row=2, max_row=_MAX_ROW_NUMBER, max_col=_MAX_COL_NUMBER):
-        row[0].value = random.choice(_COMPANY_NAME)
-        row[1].value = f"{current_date.year}-{current_date.month:02}-{current_date.day:02} " \
-                       f"{current_date.hour:02}:{current_date.month:02}:{current_date.second:02}"
-        row[2].value = random.choice(_OPERATIONS)
-        row[3].value = random.randint(100, 500000)
+    current_row_number: int = 1
 
-        current_date += timedelta(0, random.randint(0, 10))
+    while current_row_number < _MAX_ROW_NUMBER:
+        date_string: str = f"{current_date.year}-{current_date.month:02}-{current_date.day:02} " \
+            f"{current_date.hour:02}:{current_date.minute:02}:{current_date.second:02}"
 
-    wb.save(_CUSTOM_FIRST_TABLE_PATH)
+        current_row: tuple[str, str, str, int] = (
+            random.choice(_TABLE_INFORMATION["Company_name"]),
+            date_string,
+            random.choice(_TABLE_INFORMATION["Operations"]),
+            random.randint(100, 500000)
+        )
+
+        ws.append(current_row)
+
+        current_date += timedelta(seconds=random.randint(0, 10))
+        current_row_number += 1
+
+    wb.save(save_path)
 
 
 if __name__ == "__main__":
     print("Опять работать?")
-    create_table()
+    for save_path in [_CUSTOM_FIRST_TABLE_PATH, _CUSTOM_SECOND_TABLE_PATH]:
+        create_table(save_path)
     print("Дело сделано!")
